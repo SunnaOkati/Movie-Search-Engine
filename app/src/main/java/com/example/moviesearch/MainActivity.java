a2d7ec9fef320d4b65a36b079edd5a41c13d4c43
Joan Lee <joan.d.lee112@gmail.com>
2020-10-11 20:57:15 +1100

    did the homepage

--- app/src/main/java/com/example/moviesearch/MainActivity.java
+++ app/src/main/java/com/example/moviesearch/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.moviesearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // gets the logo image
+        ImageView logo = findViewById(R.id.logo_image);
+
+        // sets the image for the logo from the assets.
+        try
+        {
+            // gets the input stream, loads as drawable
+            InputStream inputStream = getAssets().open("logo_app_1.png");
+            Drawable drawable = Drawable.createFromStream(inputStream, null);
+
+            // sets the imageView image
+            logo.setImageDrawable(drawable);
+
+            inputStream.close();
+        }
+        catch(IOException ex)
+        {
+            return;
+        }
     }
 }
